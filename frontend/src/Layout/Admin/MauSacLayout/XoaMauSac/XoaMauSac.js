212d71b8df618d8741e07bfce9e0692cebe71f5e
dunpv <dungpvph45119fpt.edu.vn>
2025-02-28 20:35:49 +0700

    QuanLyMauSacUpdate

---
+++ frontend/src/Layout/Admin/MauSacLayout/XoaMauSac/XoaMauSac.js
@@ -0,0 +1,11 @@
+function XoaMauSac() {
+  const handleXoaMauSac = async () => {
+    try {
+    } catch (error) {
+      console.error("lỗi xóa màu sắc:", error);
+    }
+  };
+  return <></>;
+}
+
+export default XoaMauSac;
