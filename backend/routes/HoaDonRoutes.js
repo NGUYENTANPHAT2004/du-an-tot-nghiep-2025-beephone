37ca3c03c4bd646220d99e2f080a60eecfc8292e
dunpv <dungpvph45119fpt.edu.vn>
2025-03-07 21:38:21 +0700

    ThanhToanXoa

--- backend/routes/HoaDonRoutes.js
+++ backend/routes/HoaDonRoutes.js
@@ -31,6 +31,83 @@ router.get("/gethoadon", async (req, res) => {
   }
 });
 
+router.post("/deletehoaddon", async (req, res) => {
+  try {
+    const { ids } = req.body;
+    await HoaDon.hoadon.deleteMany({ _id: { $in: ids } });
+    res.json({ message: "Xóa hóa đơn thành công" });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.post("/posthoadon", async (req, res) => {
+  try {
+    const {
+      name,
+      phone,
+      sex,
+      giaotannoi,
+      address,
+      ghichu,
+      magiamgia,
+      sanphams,
+    } = req.body;
+
+    const hoadon = new HoaDon.hoadon({
+      name,
+      phone,
+      sex,
+      giaotannoi,
+      ngaymua: moment().toISOString(),
+      trangthai: "Đang xử lý",
+      tongtien: 0,
+    });
+    let tongtien = 0;
+
+    for (const sanpham of sanphams) {
+      const { idsp, soluong } = sanpham;
+      const sanpham1 = await SanPham.ChitietSp.findById(idsp);
+      hoadon.sanpham.push({ idsp, soluong, price: sanpham1.price });
+      tongtien += sanpham1.price * soluong;
+    }
+
+    hoadon.tongtien = tongtien;
+
+    if (magiamgia) {
+      const magiamgia1 = await MaGiamGia.magiamgia.findOne({ magiamgia });
+      const ngayHienTai = moment();
+      const ngayKetThuc = moment(magiamgia1.ngayketthuc);
+
+      if (ngayHienTai.isAfter(ngayKetThuc)) {
+        return res.json({ message: "Mã giảm giá đã hết hạn" });
+      }
+      const daSuDung = await HoaDon.hoadon.findOne({ phone, magiamgia });
+      if (daSuDung) {
+        return res
+          .status(400)
+          .json({ message: "Bạn đã sử dụng mã giảm giá này" });
+      }
+      hoadon.magiamgia = magiamgia;
+      const giamGia = magiamgia1.sophantram / 100;
+      hoadon.tongtien = tongtien - tongtien * giamGia;
+    }
+
+    if (giaotannoi) {
+      hoadon.address = address;
+    }
+    if (ghichu) {
+      hoadon.ghichu = ghichu;
+    }
+
+    await hoadon.save();
+    res.json(hoadon);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Lỗi trong quá trình thêm" });
+  }
+});
+
 router.post("/settrangthai/:idhoadon", async (req, res) => {
   try {
     const idhoadon = req.params.idhoadon;
@@ -77,6 +154,21 @@ router.get("/getchitiethd/:idhoadon", async (req, res) => {
       hoadonsanpham: hoadonsanpham,
     };
     res.json(hoadonjson);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.post("/timkiemhoadon", async (req, res) => {
+  try {
+    const { phone } = req.body;
+    const hoadon = await HoaDon.hoadon.find({ phone });
+    if (!hoadon) {
+      return res.json({
+        message: "Không có đơn hàng tương ứng với số điện thoại",
+      });
+    }
+    res.json(hoadon);
   } catch (error) {
     console.error(error);
   }
