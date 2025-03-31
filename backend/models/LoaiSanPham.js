b69363cf2b3dda496429b6f6d29e64535afca55e
Ngoc Dung <seehafer@gmail.com>
2025-03-31 20:52:38 +0700

    loaisanpham

--- backend/models/LoaiSanPham.js
+++ backend/models/LoaiSanPham.js
@@ -1,9 +1,9 @@
-const db = require('./db')
+const db = require("./db");
 
 const LoaiSPSchema = new db.mongoose.Schema({
   name: { type: String },
-  namekhongdau:{type:String},
-  chitietsp: [{ type: db.mongoose.Schema.Types.ObjectId, ref: 'chitietsp' }],
+  namekhongdau: { type: String },
+  chitietsp: [{ type: db.mongoose.Schema.Types.ObjectId, ref: "chitietsp" }],
   manhinh: { type: String },
   chip: { type: String },
   ram: { type: String },
@@ -13,9 +13,14 @@ const LoaiSPSchema = new db.mongoose.Schema({
   congsac: { type: String },
   hang: { type: String },
   thongtin: { type: String },
-  dungluongmay: [{ type: db.mongoose.Schema.Types.ObjectId, ref: 'dungluong' }],
-  category: { type: db.mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }
-})
+  dungluongmay: [{ type: db.mongoose.Schema.Types.ObjectId, ref: "dungluong" }],
 
-const LoaiSP = db.mongoose.model('loaisp', LoaiSPSchema)
-module.exports = { LoaiSP }
+  category: {
+    type: db.mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+});
+
+const LoaiSP = db.mongoose.model("loaisp", LoaiSPSchema);
+module.exports = { LoaiSP };
