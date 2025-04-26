a8664024cb233c88ba4e22df5d57b79d960a630e
duong <duongpcph37633@fpt.edu.vn>
2025-04-26 17:41:16 +0700

    update

--- frontend/src/Layout/Admin/BlogLayout/AddBlog/AddBlog.js
+++ frontend/src/Layout/Admin/BlogLayout/AddBlog/AddBlog.js
@@ -1,73 +1,73 @@
-import { Modal } from '../../../../components/Modal'
-import { useState } from 'react'
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
+import { Modal } from "../../../../components/Modal";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-function AddBlog ({ isOpen, onClose, fetchdata }) {
-  const [tieude_blog, settieude_blog] = useState('')
-  const [file, setFile] = useState(null)
-  const [noidung, setnoidung] = useState('')
+function AddBlog({ isOpen, onClose, fetchdata }) {
+  const [tieude_blog, settieude_blog] = useState("");
+  const [file, setFile] = useState(null);
+  const [noidung, setnoidung] = useState("");
   const handelclose = () => {
-  settieude_blog('')
-  setnoidung('')
-  setFile(null)
-  onClose()
-}
+    settieude_blog("");
+    setnoidung("");
+    setFile(null);
+    onClose();
+  };
 
   const handelAddBlog = async () => {
     try {
-      const formData = new FormData() // Tạo FormData
-      formData.append('tieude_blog', tieude_blog)
-      formData.append('noidung', noidung)
+      const formData = new FormData(); // Tạo FormData
+      formData.append("tieude_blog", tieude_blog);
+      formData.append("noidung", noidung);
 
       if (file) {
-        formData.append('image', file)
+        formData.append("image", file);
       }
 
-      const response = await fetch('http://localhost:3005/postblog', {
-        method: 'POST',
-        body: formData
-      })
+      const response = await fetch("http://localhost:3005/postblog", {
+        method: "POST",
+        body: formData,
+      });
       if (response.ok) {
-        handelclose()
-        fetchdata()
+        handelclose();
+        fetchdata();
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
   return (
     <Modal isOpen={isOpen} onClose={handelclose}>
-      <div className='addtheloai'>
+      <div className="addtheloai">
         <h2>Thêm Blog</h2>
-        <div className='input-group'>
+        <div className="input-group">
           <label> Ảnh</label>
-          <input type='file' onChange={e => setFile(e.target.files[0])} />
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
 
           <label>Tiêu đề:</label>
           <input
-            type='text'
+            type="text"
             value={tieude_blog}
-            onChange={e => settieude_blog(e.target.value)}
-            placeholder='Nhập tiêu đề'
+            onChange={(e) => settieude_blog(e.target.value)}
+            placeholder="Nhập tiêu đề"
           />
           <label>Nội dung:</label>
           <ReactQuill
             value={noidung}
             onChange={setnoidung}
-            placeholder='Nhập nội dung'
-            theme='snow'
+            placeholder="Nhập nội dung"
+            theme="snow"
           />
         </div>
 
-        <div className='button-group'>
-          <button onClick={handelAddBlog} className='btnaddtl'>
+        <div className="button-group">
+          <button onClick={handelAddBlog} className="btnaddtl">
             Thêm
           </button>
         </div>
       </div>
     </Modal>
-  )
+  );
 }
 
-export default AddBlog
+export default AddBlog;
