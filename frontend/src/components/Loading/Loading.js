4d72cd02964b2bed259ef635fb47ae7a38845c72
dungvdph41374 <150582676+dungvdph41374@users.noreply.github.com>
2025-05-05 17:56:17 +0700

    loading

--- frontend/src/components/Loading/Loading.js
+++ frontend/src/components/Loading/Loading.js
@@ -1,13 +1,19 @@
-// Loading.js
-import React from 'react';
-import './Loading.scss';
+const multer = require('multer')
+const path = require('path')
+const pathToUploads = path.resolve(__dirname, '../uploads')
 
-const Loading = () => {
-  return (
-    <div className="loading-overlay">
-      <div className="loading-spinner"></div>
-    </div>
-  );
-};
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, pathToUploads)
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${Math.round(
+      Math.random() * 1e9
+    )}${path.extname(file.originalname)}`
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
 
-export default Loading;
+const upload = multer({ storage: storage })
+
+module.exports = upload
